e98edbd9288cebc11b50e5bc2b01bffe44fae698
soareslss <soareslssa@fab.mil.br>
2023-10-03 13:41:13 -0300

    feat: implement localstorage

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default {
   ,
   created() {
     this.$store.dispatch('loadProducts')
+    this.$store.dispatch('loadBag')
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,17 @@ export default createStore({
     loadProducts(state, products) {
       state.products = products;
     },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
     addToBag(state, product) {
       state.productsInBag.push(product);
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag))
     },
     removeFromBag(state, productId) {
       const updatedBag = state.productsInBag.filter(item => productId != item.id);
       state.productsInBag = updatedBag;
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag))
     }
   },
   actions: {
@@ -25,6 +30,11 @@ export default createStore({
       .then((response) => {
         commit("loadProducts", response.data);
       });
+    },
+    loadBag({ commit}) {
+      if(localStorage.getItem('productsInBag')){
+        commit('loadBag', JSON.parse(localStorage.getItem('productsInBag')))
+      }
     },
     addToBag({ commit }, product) {
       commit("addToBag", product);
